7aacda13f24fd1f7cf7ee8cba7610d8dc5452ca6
usu2068 <administrador@ustarizabogados.com>
2025-05-19 15:04:58 -0500

    archivo de seguridad para cifrar automaticamente las cookies de la aplicacion - KP

--- app/Http/Middleware/EncryptCookies.php
+++ app/Http/Middleware/EncryptCookies.php
@@ -4,14 +4,36 @@ namespace App\Http\Middleware;
 
 use Illuminate\Cookie\Middleware\EncryptCookies as Middleware;
 
+/**
+ * Middleware para cifrar automáticamente las cookies en la aplicación.
+ *
+ * Esta clase extiende el middleware base `EncryptCookies` de Laravel y se utiliza para 
+ * garantizar que todas las cookies salientes estén cifradas, excepto aquellas 
+ * explícitamente especificadas en la propiedad `$except`.
+ *
+ * Laravel desencripta automáticamente las cookies entrantes si fueron previamente cifradas.
+ *
+ * @package App\Http\Middleware
+ */
 class EncryptCookies extends Middleware
 {
+
+    /**
+     * Lista de nombres de cookies que no deben ser cifradas.
+     *
+     * Si necesitas que alguna cookie esté exenta del cifrado por cualquier razón 
+     * (por ejemplo, interoperabilidad con servicios externos), puedes incluir 
+     * su nombre en este arreglo.
+     *
+     * @var array<int, string>
+     */
+
     /**
      * The names of the cookies that should not be encrypted.
      *
      * @var array
      */
     protected $except = [
-        //
+        // Agrega aquí los nombres de cookies que no deban cifrarse
     ];
 }
