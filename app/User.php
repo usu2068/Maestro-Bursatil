79310259ea9b9fe43465918a744c5473c3940f2c
usu2068 <administrador@ustarizabogados.com>
2025-05-19 16:56:54 -0500

    KP - modelo del usuario del sistema, define estructura y comportamiento del modelo user

--- app/User.php
+++ app/User.php
@@ -5,9 +5,25 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+/**
+ * Modelo User.
+ *
+ * Representa a los usuarios autenticables del sistema. Esta clase extiende la clase base
+ * `Authenticatable` de Laravel e incluye soporte para notificaciones.
+ *
+ * Está asociada a la tabla personalizada `mb04_users`.
+ *
+ * @package App
+ */
+
 class User extends Authenticatable
 {
 
+    /**
+     * Nombre de la tabla asociada al modelo.
+     *
+     * @var string
+     */
     protected $table = 'mb04_users';
 
     use Notifiable;
@@ -17,8 +33,19 @@ class User extends Authenticatable
      *
      * @var array
      */
+
+     /**
+     * Atributos que pueden ser asignados de forma masiva (mass assignment).
+     *
+     * Estos campos se pueden llenar usando métodos como `create()` o `fill()`.
+     *
+     * @var array
+     */
     protected $fillable = [
-        'name', 'email', 'password', 'cedula',
+        'name', //nombre del usuario
+        'email', // correo electronico
+        'password', //contraseña (encriptada)
+        'cedula', //Documento de identificación
     ];
 
     /**
@@ -26,7 +53,16 @@ class User extends Authenticatable
      *
      * @var array
      */
+
+     /**
+     * Atributos que deben ocultarse al serializar el modelo.
+     *
+     * Esto evita que se expongan datos sensibles cuando el modelo se convierte en array o JSON.
+     *
+     * @var array
+     */
     protected $hidden = [
-        'password', 'remember_token',
+        'password', //contraseña del usuario
+        'remember_token', //token de "recordarme" para autenticacion persistente
     ];
 }
