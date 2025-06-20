e881849a8e5c2d8cf321f63f1fd687314828c59b
usu2068 <administrador@ustarizabogados.com>
2025-06-20 13:36:37 -0500

    modelo de BD que define un tutorial con nombre y caracateristicas para asociar multiples temas a un tutorial - KP

--- app/modelos/Tutorial.php
+++ app/modelos/Tutorial.php
@@ -4,14 +4,50 @@ namespace App\modelos;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Modelo Tutorial
+ * 
+ * Representa un tutorial dentro de la aplicación.
+ * 
+ * Tabla: mb04_tutoria
+ * 
+ * Propósito:
+ * - Definir un tutorial con su nombre y características.
+ * - Asociar múltiples temas a un tutorial.
+ * 
+ * Atributos:
+ * - nombre (string)
+ * - caracteristica (string)
+ * 
+ * Relaciones:
+ * - Muchos a muchos con ContenidoTuto (temas del tutorial)
+ */
+
 class Tutorial extends Model{
 	
+    /**
+     * Nombre de la tabla en la base de datos.
+     * 
+     * @var string
+     */
+
     public $table = "mb04_tutoria";
+
+    /**
+     * Campos que se pueden asignar de manera masiva (mass assignable).
+     * 
+     * @var array
+     */
 
     protected $fillable = [
         'nombre', 'caracteristica'
     ];
 
+    /**
+     * Relación: Temas asociados a este tutorial.
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
     public function temsTuto(){
 
         return $this->belongsToMany('App\modelos\ContenidoTuto', 'mb04_temasOfTutoria', 'id_tutoria', 'id_contenido');
