4cddcdc90bee67e2fe45f323e62f667388ba7210
usu2068 <administrador@ustarizabogados.com>
2025-06-20 14:20:54 -0500

    archivo factory usado para generar datos de prueba o de relleno de forma automatica en la base de datos - KP

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -11,6 +11,7 @@ use Faker\Generator as Faker;
 | your application. Factories provide a convenient way to generate new
 | model instances for testing / seeding your application's database.
 |
+|
 */
 
 $factory->define(App\modelos\User::class, function (Faker $faker) {
