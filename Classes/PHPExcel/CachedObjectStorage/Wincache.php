7b30e578603738fb561fd8d35be8342560b1c1b0
usu2068 <administrador@ustarizabogados.com>
2025-06-20 14:08:32 -0500

    cachedObjectStorage son librerias que utiliza PHP para el alamacenamiento de informacion en memoria - KP

--- Classes/PHPExcel/CachedObjectStorage/Wincache.php
+++ Classes/PHPExcel/CachedObjectStorage/Wincache.php
@@ -19,6 +19,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  *
+ * 
  * @category   PHPExcel
  * @package    PHPExcel_CachedObjectStorage
  * @copyright  Copyright (c) 2006 - 2015 PHPExcel (http://www.codeplex.com/PHPExcel)
