2e8d71b1a0349cf5bff6361f77324f5e22ae8da1
usu2068 <administrador@ustarizabogados.com>
2025-06-20 13:31:30 -0500

    modelo de BD que representa la relacion entre un tutoria y los temas que lo componen - KP

--- app/modelos/TemasOfTutoria.php
+++ app/modelos/TemasOfTutoria.php
@@ -4,9 +4,37 @@ namespace App\modelos;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Modelo TemasOfTutoria
+ * 
+ * Representa la relación entre un Tutorial y los Temas que lo componen.
+ * 
+ * Tabla: mb04_temasOfTutoria
+ * 
+ * Propósito:
+ * - Permite organizar qué temas pertenecen a cada tutorial.
+ * - Soporta la navegación por temas dentro de un tutorial.
+ * 
+ * Relaciones:
+ * - Pertenece a Tutorial
+ * - Pertenece a ContenidoTuto (tema)
+ */
+
 class TemasOfTutoria extends Model{
 
+    /**
+     * Nombre de la tabla en la base de datos.
+     * 
+     * @var string
+     */
+
     public $table = "mb04_temasOfTutoria";
+
+    /**
+     * Relación: Este tema pertenece a un tutorial.
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
 
     public function tutoria(){
 
@@ -14,6 +42,12 @@ class TemasOfTutoria extends Model{
 
     }
 
+    /**
+     * Relación: Tema asociado a esta relación.
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    
     public function tema(){
 
         return $this->belongsTo(ContenidoTuto::class, 'id_contenido');
