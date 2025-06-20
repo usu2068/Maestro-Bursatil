63fd607f2444c613726e8504408fd6e310c69cb0
usu2068 <administrador@ustarizabogados.com>
2025-06-20 13:26:39 -0500

    modelo de BD que gestiona lla relacion entre el simulador y los temas que contiene cada uno - KP

--- app/modelos/TemasOfSimulador.php
+++ app/modelos/TemasOfSimulador.php
@@ -4,13 +4,52 @@ namespace App\modelos;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Clase TemasOfSimulador
+ * 
+ * Este modelo representa la tabla intermedia "mb04_temasOfSimulador",
+ * que relaciona los simuladores con los temas que contiene cada simulador.
+ * 
+ * Es un modelo típico para gestionar relaciones muchos a muchos, con un campo adicional:
+ * 
+ * Atributos:
+ * - id_simulador: ID del simulador al que pertenece este tema.
+ * - id_tema: ID del tema (ContenidoSimu) asociado.
+ * - NoPreg: Número de preguntas que se tomarán de este tema para el simulador.
+ * 
+ * Relaciones:
+ * - simulador(): Relación inversa hacia el simulador (Simulador).
+ * - tema(): Relación hacia el tema de contenido (ContenidoSimu).
+ * 
+ * Nota: aunque la relación a tema se implementa con "hasOne", 
+ * normalmente en relaciones intermedias sería más común usar "belongsTo" (como tienes comentado).
+ */
+
 class TemasOfSimulador extends Model{
 
+    /**
+     * Nombre de la tabla asociada a este modelo.
+     * 
+     * @var string
+     */
+
     public $table = "mb04_temasOfSimulador";
+
+    /**
+     * Atributos que se pueden asignar de forma masiva.
+     * 
+     * @var array
+     */
 
     protected $fillable = [
         'id_simulador', 'id_tema', 'NoPreg',
     ];
+
+    /**
+     * Relación: Simulador al que pertenece este tema.
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
 
     public function simulador(){
 
@@ -18,6 +57,14 @@ class TemasOfSimulador extends Model{
 
     }
 
+    /**
+     * Relación: Tema asociado a este registro.
+     * 
+     * Actualmente definido como "hasOne", aunque "belongsTo" también sería válido.
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    
     public function tema(){
 
         //return $this->belongsTo(ContenidoSimu::class, 'id_contenido');
